88720bdc285f8641054619d81d39319725699fe6
Pierre Colle <piercus@gmail.com>
2023-02-20 09:37:55 +0100

    fix: add npx build-opencv

--- guest/package-guest-install.bash
+++ guest/package-guest-install.bash
@@ -6,5 +6,7 @@ UID1=`stat -c '%u' .`
 GID1=`stat -c '%g' .`
 echo "chown -R $UID1:$GID1 /root"
 chown -R $UID1:$GID1 "/root"
-echo "Npm Install"
+echo "Run: npm install"
 npm install
+echo "Run: npx build-opencv rebuild"
+npx build-opencv rebuild
