3b3e72f19192e53c1b4a7e36c4a24930d03265b2
Colle <piercus@users.noreply.github.com>
2023-02-21 18:33:42 +0100

    Update package-guest-install.bash

--- guest/package-guest-install.bash
+++ guest/package-guest-install.bash
@@ -10,6 +10,5 @@ echo "chown -R $UID1:$GID1 /root"
 chown -R $UID1:$GID1 "/root"
 echo "Run: npm install  --foreground-scripts"
 npm install --foreground-scripts
-echo "Run: npx build-opencv build"
-export OPENCV4NODEJS_AUTOBUILD_OPENCV_VERSION="${OPENCV_VERSION}"
-npx build-opencv rebuild 
+echo "Run: npx build-opencv --version \"${OPENCV_VERSION}\" build"
+npx build-opencv --version "${OPENCV_VERSION}" rebuild 
