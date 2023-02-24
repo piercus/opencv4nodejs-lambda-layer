57df27dc226696498c4ceb8c3143002143222e17
Pierre Colle <piercus@gmail.com>
2023-02-24 22:46:57 +0100

    fix: activate flann and features2d to have calib3d

--- guest/package-guest-test.bash
+++ guest/package-guest-test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-echo "Run: require('@u4/opencv4nodejs'); console.log('require @u4/opencv4nodejs is sucessful')"
-node -e "require('@u4/opencv4nodejs'); console.log('require @u4/opencv4nodejs is sucessful')"
+echo "Run Nodejs : const cv = require('@u4/opencv4nodejs'); if(typeof(cv.solvePnP) === 'function'){ console.log('require @u4/opencv4nodejs is sucessful')} else {throw(new Error('cv.solvePnP is not a function'))}"
+node -e "const cv = require('@u4/opencv4nodejs'); if(typeof(cv.solvePnP) === 'function'){ console.log('require @u4/opencv4nodejs is sucessful')} else {throw(new Error('cv.solvePnP is not a function'))}"
 
